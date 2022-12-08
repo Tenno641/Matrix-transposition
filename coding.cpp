38e9b24e5de4edab58bdad561a8f7142ca148725
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2022-12-08 15:53:09 +0200

    The matrix elements
    getting the matrix elements from the user

--- coding.cpp
+++ coding.cpp
@@ -7,5 +7,12 @@ int main() {
     int rows, cols;
     cout << "Enter the size of the matrix\n";
     cin >> rows >> cols;
+    // Matrix inputs
+    int arr[rows][cols];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
     return 0;
 }
