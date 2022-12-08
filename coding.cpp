c4b93e8d524d29e51dddf675a7f00b74339ea81a
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2022-12-08 15:51:21 +0200

    initialising the matrix size.
    getting the matrix size from the user

--- coding.cpp
+++ coding.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main() {
     
+    //Initialising the matrix
+    int rows, cols;
+    cout << "Enter the size of the matrix\n";
+    cin >> rows >> cols;
     return 0;
 }
